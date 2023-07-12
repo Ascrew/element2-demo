27315a9ab2f060aa3c6c797ce3d5509d089ad85b
zhangyu <1311585620@qq.com>
2023-07-12 14:26:07 +0800

    feat: 上传第一个组件Loading

--- examples/App.vue
+++ examples/App.vue
@@ -17,9 +17,20 @@ export default {
 
 <style>
 #app {
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#nav {
+  padding: 30px;
+}
+a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- examples/main.js
+++ examples/main.js
@@ -3,7 +3,20 @@ import App from "./App.vue";
 import router from "./router";
 import MeUI from "../src/index";
 
+import hljs from "highlight.js";
+import DemoBlock from "./components/demo-block";
+import "highlight.js/styles/stackoverflow-light.css";
+
+Vue.component("demo-block", DemoBlock);
 Vue.use(MeUI);
+
+router.afterEach(() => {
+  Vue.nextTick(() => {
+    const blocks = document.querySelectorAll("pre code:not(.hljs)");
+    Array.prototype.forEach.call(blocks, hljs.highlightBlock);
+  });
+  document.title = "me-ui";
+});
 
 new Vue({
   router,
